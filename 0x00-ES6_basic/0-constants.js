58a7475c67a528955326f513f0564155acbebe4c
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-17 08:23:20 -0700

    double quotes removed

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,16 @@
 export function taskFirst() {
-    const task = "I prefer const when I can.";
+    const task = 'I prefer const when I can.';
     return task;
 
 }
 
 export function getLast() {
-    return " is okay";
+    return ' is okay';
 }
 
 export function taskNext() {
-    let combination = "But sometimes let";
+    let combination = 'But sometimes let';
     combination = combination + getLast();
+
     return combination;
 }
